41c5df0d085d981a14235f5885343d3ddfa4c45a
Ryxwaer <ryxwaer@gmail.com>
2021-10-16 19:28:05 +0200

    autostart files changed to .desktop

--- install.sh
+++ install.sh
@@ -7,8 +7,12 @@ sed -i "s/XXXXX/$HOME/g" desktop.gamecontroller.amgp
 
 sudo cp "desktop.gamecontroller.amgp" "$HOME/AntiMicro/desktop.gamecontroller.amgp"
 sudo chmod +x "$HOME/AntiMicro/desktop.gamecontroller.amgp"
-sudo cp "toggleOnboard.sh" "~/AntiMicro/toggleOnboard.sh"
+sudo cp "toggleOnboard.sh" "$HOME/AntiMicro/toggleOnboard.sh"
 sudo chmod +x "$HOME/AntiMicro/toggleOnboard.sh"
+sudo cp "io.github.antimicrox.antimicrox.desktop" "$HOME/A.config/autostart/io.github.antimicrox.antimicrox.desktop"
+sudo chmod +x "$HOME/.config/autostart/io.github.antimicrox.antimicrox.desktop"
+sudo cp "onboard.desktop" "$HOME/A.config/autostart/onboard.desktop"
+sudo chmod +x "$HOME/.config/autostart/onboard.desktop"
 
 sudo cp "controllerAutostart.sh" "$HOME/.config/autostart/controllerAutostart.sh"
 sudo chmod +x "$HOME/.config/autostart/controllerAutostart.sh"
